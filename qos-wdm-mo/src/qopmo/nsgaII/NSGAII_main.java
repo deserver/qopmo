381cbc28d28415cfd7a21b242c5c774af7b4743c
sergio <deserver123@gmail.com>
2015-12-31 16:46:53 -0300

    Modificaciones en el Ranking

--- qos-wdm-mo/src/qopmo/nsgaII/NSGAII_main.java
+++ qos-wdm-mo/src/qopmo/nsgaII/NSGAII_main.java
@@ -81,97 +81,99 @@ public class NSGAII_main {
                                   SecurityException, 
                                   IOException, 
                                   ClassNotFoundException {
-    Problem   problem   ; // The problem to solve
-    Algorithm algorithm ; // The algorithm to use
-    Operator  crossover ; // Crossover operator
-    Operator  mutation  ; // Mutation operator
-    Operator  selection ; // Selection operator
-    
-    HashMap  parameters ; // Operator parameters
-    
-    QualityIndicator indicators ; // Object to get quality indicators
-
-    // Logger object and file to store log messages
-    logger_      = Configuration.logger_ ;
-    fileHandler_ = new FileHandler("NSGAII_main.log"); 
-    logger_.addHandler(fileHandler_) ;
-        
-    indicators = null ;
-    if (args.length == 1) {
-      Object [] params = {"Real"};
-      problem = (new ProblemFactory()).getProblem(args[0],params);
-    } // if
-    else if (args.length == 2) {
-      Object [] params = {"Real"};
-      problem = (new ProblemFactory()).getProblem(args[0],params);
-      indicators = new QualityIndicator(problem, args[1]) ;
-    } // if
-    else { // Default problem
-      //problem = new Kursawe("Real", 3);
-      //problem = new Kursawe("BinaryReal", 3);
-      //problem = new Water("Real");
-      //problem = new ZDT3("ArrayReal", 30);
-      //problem = new ConstrEx("Real");
-      //problem = new DTLZ1("Real");
-      //problem = new OKA2("Real") ;
-      problem = new QOP();
-    } // else
-    
-    algorithm = new NSGAII(problem, 1);
-    //algorithm = new ssNSGAII(problem);
-
-    // Algorithm parameters
-    algorithm.setInputParameter("populationSize",5);
-    algorithm.setInputParameter("maxEvaluations",5);
-
-    // Mutation and Crossover for Real codification 
-    /*parameters = new HashMap() ;
-    parameters.put("probability", 0.9) ;
-    parameters.put("distributionIndex", 20.0) ;
-    crossover = CrossoverFactory.getCrossoverOperator("SBXCrossover", parameters);                   
-
-    parameters = new HashMap() ;
-    parameters.put("probability", 1.0/problem.getNumberOfVariables()) ;
-    parameters.put("distributionIndex", 20.0) ;
-    mutation = MutationFactory.getMutationOperator("PolynomialMutation", parameters);                    
-
-    // Selection Operator 
-    parameters = null ;
-    selection = SelectionFactory.getSelectionOperator("BinaryTournament2", parameters) ;  
-    */
-    
-    // Add the operators to the algorithm
-    /*algorithm.addOperator("crossover",crossover);
-    algorithm.addOperator("mutation",mutation);
-    algorithm.addOperator("selection",selection);*/
-    //algorithm.addOperator("torneobinario", torneobinario);
-
-    // Add the indicator object to the algorithm
-    algorithm.setInputParameter("indicators", indicators) ;
-    
-    // Execute the Algorithm
-    long initTime = System.currentTimeMillis();
-    Poblacion population = algorithm.execute();
-    long estimatedTime = System.currentTimeMillis() - initTime;
-    
-    System.out.println("FIN Prueba Algoritmo Genetico. (Link-Oriented)");
-    // Result messages 
-    logger_.info("Total execution time: "+estimatedTime + "ms");
-    logger_.info("Variables values have been writen to file VAR");
-    population.printVariablesToFile("VAR");    
-    logger_.info("Objectives values have been writen to file FUN");
-    population.printObjectivesToFile("FUN");
-  	
-    /*if (indicators != null) {
-      logger_.info("Quality indicators") ;
-      logger_.info("Hypervolume: " + indicators.getHypervolume(population)) ;
-      logger_.info("GD         : " + indicators.getGD(population)) ;
-      logger_.info("IGD        : " + indicators.getIGD(population)) ;
-      logger_.info("Spread     : " + indicators.getSpread(population)) ;
-      logger_.info("Epsilon    : " + indicators.getEpsilon(population)) ;  
-     
-      int evaluations = ((Integer)algorithm.getOutputParameter("evaluations")).intValue();
-      logger_.info("Speed      : " + evaluations + " evaluations") ;      
-    } // if*/
+	  for (int l=0; l<10; l++){
+	    Problem   problem   ; // The problem to solve
+	    Algorithm algorithm ; // The algorithm to use
+	    Operator  crossover ; // Crossover operator
+	    Operator  mutation  ; // Mutation operator
+	    Operator  selection ; // Selection operator
+	    
+	    HashMap  parameters ; // Operator parameters
+	    
+	    QualityIndicator indicators ; // Object to get quality indicators
+	
+	    // Logger object and file to store log messages
+	    logger_      = Configuration.logger_ ;
+	    fileHandler_ = new FileHandler("NSGAII_main.log"); 
+	    logger_.addHandler(fileHandler_) ;
+	        
+	    indicators = null ;
+	    if (args.length == 1) {
+	      Object [] params = {"Real"};
+	      problem = (new ProblemFactory()).getProblem(args[0],params);
+	    } // if
+	    else if (args.length == 2) {
+	      Object [] params = {"Real"};
+	      problem = (new ProblemFactory()).getProblem(args[0],params);
+	      indicators = new QualityIndicator(problem, args[1]) ;
+	    } // if
+	    else { // Default problem
+	      //problem = new Kursawe("Real", 3);
+	      //problem = new Kursawe("BinaryReal", 3);
+	      //problem = new Water("Real");
+	      //problem = new ZDT3("ArrayReal", 30);
+	      //problem = new ConstrEx("Real");
+	      //problem = new DTLZ1("Real");
+	      //problem = new OKA2("Real") ;
+	      problem = new QOP();
+	    } // else
+	    
+	    algorithm = new NSGAII(problem, 40);
+	    //algorithm = new ssNSGAII(problem);
+	
+	    // Algorithm parameters
+	    algorithm.setInputParameter("populationSize",50);
+	    algorithm.setInputParameter("maxEvaluations",50);
+	
+	    // Mutation and Crossover for Real codification 
+	    /*parameters = new HashMap() ;
+	    parameters.put("probability", 0.9) ;
+	    parameters.put("distributionIndex", 20.0) ;
+	    crossover = CrossoverFactory.getCrossoverOperator("SBXCrossover", parameters);                   
+	
+	    parameters = new HashMap() ;
+	    parameters.put("probability", 1.0/problem.getNumberOfVariables()) ;
+	    parameters.put("distributionIndex", 20.0) ;
+	    mutation = MutationFactory.getMutationOperator("PolynomialMutation", parameters);                    
+	
+	    // Selection Operator 
+	    parameters = null ;
+	    selection = SelectionFactory.getSelectionOperator("BinaryTournament2", parameters) ;  
+	    */
+	    
+	    // Add the operators to the algorithm
+	    /*algorithm.addOperator("crossover",crossover);
+	    algorithm.addOperator("mutation",mutation);
+	    algorithm.addOperator("selection",selection);*/
+	    //algorithm.addOperator("torneobinario", torneobinario);
+	
+	    // Add the indicator object to the algorithm
+	    algorithm.setInputParameter("indicators", indicators) ;
+	    
+	    // Execute the Algorithm
+	    long initTime = System.currentTimeMillis();
+	    Poblacion population = algorithm.execute();
+	    long estimatedTime = System.currentTimeMillis() - initTime;
+	    
+	    System.out.println("FIN Prueba Algoritmo Genetico. (Link-Oriented)");
+	    // Result messages 
+	    logger_.info("Total execution time: "+estimatedTime + "ms");
+	    logger_.info("Variables values have been writen to file VAR");
+	    population.printVariablesToFile("VAR");    
+	    logger_.info("Objectives values have been writen to file FUN");
+	    population.printObjectivesToFile("FUN");
+	  	
+	    /*if (indicators != null) {
+	      logger_.info("Quality indicators") ;
+	      logger_.info("Hypervolume: " + indicators.getHypervolume(population)) ;
+	      logger_.info("GD         : " + indicators.getGD(population)) ;
+	      logger_.info("IGD        : " + indicators.getIGD(population)) ;
+	      logger_.info("Spread     : " + indicators.getSpread(population)) ;
+	      logger_.info("Epsilon    : " + indicators.getEpsilon(population)) ;  
+	     
+	      int evaluations = ((Integer)algorithm.getOutputParameter("evaluations")).intValue();
+	      logger_.info("Speed      : " + evaluations + " evaluations") ;      
+	    } // if*/
+	  }//for
   } //main
 } // NSGAII_main
